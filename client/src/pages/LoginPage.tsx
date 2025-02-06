d22810b260b382d9c352bc6a2812a2316859ffa3
Paige <paige.renee395@gmail.com>
2025-02-05 23:06:31 -0600

    Pushing the changes I have so far with the landing page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
+import { Modal, Button } from 'antd';
 import Nav from '../components/Nav';
+
+const [isLoginVisible, setLoginVisible] = useState(false);
+const [isSignupVisible, setSignupVisible] = useState(false);
+
+const showLogin = () => {
+  setLoginVisible(true);
+};
+
+const showSignup = () => {
+  setSignupVisible(true);
+};
+
+const handleLogin = () => {
+  //add login logic
+  setLoginVisible(false);
+};
+
+const handleSignup = () => {
+  //add signup logic
+  setSignupVisible(false);
+};
 
 const LoginPage = () => {
   return (
-    <div>
-      <Nav />
-      <p>Will change this page to ask user to log in or sign up</p>
-    </div>
+    <>
+      <div>
+        <Nav />
+      </div>
+      <main>
+        <h2>Log in or sign up to start planning today!</h2>
+        <Button onClick={showSignup}>Sign Up</Button>
+        <Button onClick={showLogin}>Log In</Button>
+      </main>
+    </>
   );
 };
 
