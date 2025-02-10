d2fd6f7af85320907cbcf1e8b331f5772bc176c8
Paige <paige.renee395@gmail.com>
2025-02-10 03:53:48 -0600

    I had to make a new branch. I removed the caterer model and fixed the other models and routes.

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './user-routes.js';
+import { eventRouter } from './event-routes.js';
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/events', eventRouter);
 
 export default router;
