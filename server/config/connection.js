878134ed28bfedc23ee4a8be2182bdb38a4637bc
Paige <paige.renee395@gmail.com>
2025-02-11 12:51:26 -0600

    Fixed being able to hide postgres password

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,18 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const sequelize = new Sequelize(
   'database_development', // Database name
   'postgres', // Username
-  'PaigeR@2005', // Password
+  process.env.DB_PASSWORD, // Password
   {
     host: 'localhost',
     dialect: 'postgres',
