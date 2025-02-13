bd75a3999bb2f37c52926c9fb08ad0649af77fd1
Paige <paige.renee395@gmail.com>
2025-02-13 16:29:30 -0600

    Committing changes for the error fixes I've tried to do so far

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const forceDatabaseRefresh = false;
 
 import express from 'express';
 import sequelize from './config/connection.js';
-import routes from './routes/index.js';
+import { router } from '../server/routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,17 +10,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('../client/dist'));
 
 app.use(express.json());
-app.use(routes);
+app.use(router);
 
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
-
-// import { dirname } from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// dotenv.config({ path: path.resolve(__dirname, '../../.env') });
